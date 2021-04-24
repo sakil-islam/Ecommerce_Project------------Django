f1f85e1611d0acae60b878476b9cee3e55f1cdff
sakil-islam <77716390+sakil-islam@users.noreply.github.com>
2021-04-25 01:44:01 +0600

    Home Page Add Product create views, models, shop categories

--- App_Login/views.py
+++ App_Login/views.py
@@ -36,8 +36,7 @@ def login_user(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponse('Logged in')
-                # return HttpResponseRedirect(reverse('App_Shop:home'))
+                return HttpResponseRedirect(reverse('App_Shop:home'))
     return render(request, 'App_Login/login.html', context={'form':form})
 
 
@@ -45,8 +44,7 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     messages.warning(request, "You are logged out!!")
-    return HttpResponse('Logged out')
-    #return HttpResponseRedirect(reverse('App_Shop:home'))
+    return HttpResponseRedirect(reverse('App_Shop:home'))
 
 
 @login_required
